2f08135d017c762ec6e62daf1e3b3df14021c4d8
Hayden Buckley-Smith <hayden@abstack.co>
2022-04-25 15:00:18 +0100

    chore: State
    defined state with many fields including step
    create 3 methods (prevStep, nextStep and handleChange)

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
-
 export default class Signup extends Component {
+  state = {
+    step: 1,
+    email: "",
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    country: "",
+    levelOfEducation: "",
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step - 1 });
+  };
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step + 1 });
+  };
+
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
   render() {
     return (
       <>
