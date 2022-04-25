361fe0cab400f0baf10148ba653244b54b25eb82
Hayden Buckley-Smith <hayden@abstack.co>
2022-04-25 15:03:07 +0100

    chore: Import Components
    import and passed state to the sub components to parent component Signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import Confirmation from "./Confirmation/Confirmation";
+import PersonalDetails from "./PersonalDetails/PersonalDetails";
+import Success from "./Success/Success";
+import UserDetails from "./UserDetails/UserDetails";
+
 export default class Signup extends Component {
   state = {
     step: 1,
@@ -26,10 +31,48 @@ export default class Signup extends Component {
   };
 
   render() {
-    return (
-      <>
-        <></>
-      </>
-    );
+    const { step } = this.state;
+    const {
+      email,
+      username,
+      password,
+      firstName,
+      lastName,
+      country,
+      levelOfEducation,
+    } = this.state;
+    const values = { email, username, password, firstName, lastName, country };
+
+    switch (step) {
+      case 1:
+        return (
+          <UserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <PersonalDetails
+            prevStep={this.prevStep}
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirmation
+            prevStep={this.prevStep}
+            nextStep={this.nextStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+      default:
+        break;
+    }
   }
 }
