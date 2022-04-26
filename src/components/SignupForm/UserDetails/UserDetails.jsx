6fb250ef924c32c76225554013344ec9434d7216
Hayden Buckley-Smith <hayden@abstack.co>
2022-04-26 14:58:12 +0100

    chore: UserDetails Component
    Add intro to start of page
    add p tag to mention about having an account already option
    styled component

--- src/components/SignupForm/UserDetails/UserDetails.jsx
+++ src/components/SignupForm/UserDetails/UserDetails.jsx
@@ -7,39 +7,59 @@ const UserDetails = ({ nextStep, handleChange, values }) => {
   };
 
   return (
-    <div>
-      <form className="max-w-screen-sm mx-auto px-4 space-y-2">
-        <input
-          type="text"
-          placeholder="First Name"
-          values={values.email}
-          onChange={handleChange("firstName")}
-          className="w-full"
-        />
-        <div className="">
+    <div className="space-y-8">
+      <div className="space-y-3 text-center">
+        <p>
+          For a new beginning, enter your details into the following fields.
+        </p>
+      </div>
+      <form className="max-w-screen-sm mx-auto px-24 space-y-4">
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            values={values.email}
+            onChange={handleChange("firstName")}
+            className="w-full pl-2 rounded"
+            required
+          />
+        </div>
+        <div>
           <input
             type="text"
             placeholder="Last Name"
             values={values.email}
             onChange={handleChange("lastName")}
-            className="w-full"
-          />
-        </div>
-        <div className="">
-          <input
-            type="text"
-            placeholder="Email address"
-            values={values.email}
-            onChange={handleChange("email")}
-            className="w-full"
+            className="w-full pl-2 rounded"
+            required
           />
         </div>
         <div>
-          <button onClick={Continue} type="submit">
+          <input
+            type="email"
+            placeholder="Email address"
+            values={values.email}
+            onChange={handleChange("email")}
+            className="w-full pl-2 rounded"
+            required
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            onClick={Continue}
+            type="submit"
+            className="bg-slate-900 text-white rounded py-1 px-4 hover:bg-slate-700"
+          >
             Next
           </button>
         </div>
       </form>
+      <div className="text-center">
+        <a href="/">
+          Already got an account?{" "}
+          <button className="text-red-600">Sign in</button>
+        </a>
+      </div>
     </div>
   );
 };
