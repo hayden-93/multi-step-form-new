e55526b765560f7aa13551a1e8c5d6bc3380cb80
Hayden Buckley-Smith <hayden@abstack.co>
2022-04-26 15:01:36 +0100

    feat: Confirmation Component
    add button click event to show prev and next page
    add button with styling
    add intro

--- src/components/SignupForm/Confirmation/Confirmation.jsx
+++ src/components/SignupForm/Confirmation/Confirmation.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 
-const Confirmation = () => {
+const Confirmation = ({ prevStep, nextStep, values }) => {
+  const Continue = (e) => {
+    e.preventDefault();
+    nextStep();
+  };
+
+  const Previous = (e) => {
+    e.preventDefault();
+    prevStep();
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="max-w-screen-sm mx-auto px-24 space-y-10 text-center">
+      <button>
+        Thank you for signing up with LogoIpsum. To confirm all details are
+        correct all you will need to do is submit at the bottom of the page
+      </button>
+      <p>
+        If you wish to subcribe for marketing and newsletters, please click{" "}
+        <button className="text-red-700">here</button> to be added to our
+        mailing list
+      </p>
+      <div className="flex justify-between">
+        <div>
+          <button
+            onClick={Previous}
+            type="submit"
+            className="bg-slate-900 text-white rounded py-1 px-4 hover:bg-slate-700"
+          >
+            Go Back
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={Continue}
+            type="submit"
+            className="bg-slate-900 text-white rounded py-1 px-4 hover:bg-slate-700"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
