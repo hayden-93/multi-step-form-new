a53302a7989af64263c3fcb1ea4dfee541396a8e
Hayden Buckley-Smith <hayden@abstack.co>
2022-04-27 20:14:55 +0100

    Success Component
    refactor a tag to button and set restart onclick function to revert to start of signup

--- src/components/SignupForm/Success/Success.jsx
+++ src/components/SignupForm/Success/Success.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Success = () => {
+export const Success = ({ restart }) => {
   return (
-    <div className="space-y-4 text-center">
-      <h1>You are all done! Welcome to the LogoIpsum family!</h1>
-      <a href="/">Continue to website</a>
+    <div className="py-4 text-center">
+      <h1 className="pb-4">
+        You are all done! Welcome to the LogoIpsum family!
+      </h1>
+      <button type="button" onClick={restart}>
+        Continue to website
+      </button>
     </div>
   );
 };
-
-export default Success;
